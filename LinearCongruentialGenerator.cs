fea7f8c95e85c96ec731b70d84749a17071bf11a
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-08-09 11:01:52 +0800

    add default value to seed

--- LinearCongruentialGenerator.cs
+++ LinearCongruentialGenerator.cs
@@ -2,6 +2,9 @@
 
 namespace linear_congruential_generator 
 {
+    /// <summary>
+    /// Randomizes a list of items using the Linear Congruential Method (LCM).
+    /// </summary>
     public class LinearCongruentialGenerator
     {
         private int _seed;
@@ -9,22 +12,37 @@ namespace linear_congruential_generator
         private int _c;
         private int _m;
 
-        public LinearCongruentialGenerator(int seed)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinearCongruentialGenerator"/> class.
+        /// </summary>
+        /// <param name="seed">Optional. The seed value for the random number generator. If not provided, defaults to the current system tick count.</param>
+        public LinearCongruentialGenerator(int? seed = null)
         {
-            _seed = seed;
+            _seed = seed ?? (int)DateTime.Now.Ticks % int.MaxValue;
         }
 
+        /// <summary>
+        /// Generates the random index number using the LCM formula.
+        /// </summary>
+        /// <param name="xn">The current xn value.</param>
+        /// <returns>The random index number in the sequence.</returns>
         private int GetRandomIndex(int xn)
         {
             return (_a * xn + _c) % _m;
         }
 
-        public List<T> Randomize<T>(List<T> list)
+        /// <summary>
+        /// Randomizes the specified list using the Linear Congruential Method.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="data">The list of items to randomize.</param>
+        /// <returns>A new list with the elements in a randomized order.</returns>
+        public List<T> Randomize<T>(List<T> data)
         {
-            List<T> randomizedList = new List<T>(list);
+            List<T> randomizedList = new List<T>(data);
             int randomIndex = _seed;
 
-            _m = list.Count;
+            _m = data.Count;
             _a = PrimeNumberGenerator.GetRandomPrime(1, _m);
             _c = PrimeNumberGenerator.GetRandomPrime(1, _m);
 
--- Utils/PrimeNumberGenerator.cs
+++ Utils/PrimeNumberGenerator.cs
@@ -1,9 +1,19 @@
 namespace linear_congruential_generator.Utils
 {
+
+    /// <summary>
+    /// Provides methods for generating prime numbers.
+    /// </summary>
     public class PrimeNumberGenerator
     {
         private static Random _random = new Random();
 
+        /// <summary>
+        /// Gets a random prime number between the specified minimum and maximum values.
+        /// </summary>
+        /// <param name="min">The minimum value for the prime number.</param>
+        /// <param name="max">The maximum value for the prime number.</param>
+        /// <returns>A random prime number between <paramref name="min"/> and <paramref name="max"/>.</returns>
         public static int GetRandomPrime(int min, int max)
         {
             List<int> primes = Enumerable.Range(min, max - min)
@@ -12,6 +22,12 @@ namespace linear_congruential_generator.Utils
 
             return primes[_random.Next(primes.Count)];
         }
+
+        /// <summary>
+        /// Determines whether the specified number is prime.
+        /// </summary>
+        /// <param name="number">The number to check for primality.</param>
+        /// <returns><c>true</c> if the number is prime; otherwise, <c>false</c>.</returns>
 
         private static bool IsPrime(int number)
         {
